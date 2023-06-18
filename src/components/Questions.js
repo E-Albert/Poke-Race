dee3ca8277be427c9542fd4f35105a3f570005a3
E-Albert <magicspoon123@gmail.com>
2023-06-18 17:57:25 -0400

    Created a Questions component with 10 questions in it

---
+++ src/components/Questions.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+function Questions() {
+  let pokeQuestions = [
+    {
+      title: "What type of Pokemon is Squirtle?",
+      choices: ["Fairy", "Water", "Grass", "Normal"],
+      answer: "Water",
+    },
+    {
+      title: "Who was Ash's starter Pokemon?",
+      choices: ["Pikachu", "Geodude", "Charmander", "Caterpie"],
+      answer: "Pikachu",
+    },
+    {
+      title: "Which Pokemon has a burning tail?",
+      choices: ["Charmander", "Weedle", "Pidgey", "Vulpix"],
+      answer: "Charmander",
+    },
+    {
+      title: "Which of the following is Grass type Pokemon?",
+      choices: ["Jigglypuff", "Dugtrio", "Vileplume", "Machoke"],
+      answer: "Vileplume",
+    },
+    {
+      title: "Who is the partner of Jesse from Team Rocket?",
+      choices: ["Cylde", "Samantha", "Deeno", "James"],
+      answer: "James",
+    },
+    {
+      title: "Which member of team rocket also acts as a translator?",
+      choices: ["Brock", "Meowth", "Misty", "Slowbro"],
+      answer: "Meowth",
+    },
+    {
+      title: "Where is Ash Ketchum from?",
+      choices: [
+        "Cerulean City",
+        "Pallet Town",
+        "Fuchsia City",
+        "Indigo Plateau",
+      ],
+      answer: "Pallet Town",
+    },
+    {
+      title: "Kadabra evolves into?",
+      choices: ["Abra", "Poliwrath", "Bellsprout", "Alakazam"],
+      answer: "Alakazam",
+    },
+    {
+      title: "How many types of Pokemon are there?",
+      choices: ["18", "20", "21", "16"],
+      answer: "18",
+    },
+    {
+        title: "How does Pikachu evolve into Raichu?",
+        choices: ["In a sandstorm", "During the night time", "With a Thunderstone", "With a Moonstone"],
+        answer: "With a Thunderstone",
+    },
+  ];
+    
+    return (
+        <div></div>
+    )
+}
+
+export default Questions
