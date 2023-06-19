b87bf2073ac5fd1b7133e2c7d6067d2dd126729b
E-Albert <magicspoon123@gmail.com>
2023-06-18 20:37:32 -0400

    added answer validation and way to display next question

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import './Button.css'
 
 function Button(props) {
   return (
-    <button type={props.type || "button"} onClick={props.onClick} form={props.form || ''}>
+    <button type={props.type || "button"} onClick={props.onClick} form={props.form || ''} value={props.value || ''}>
       {props.children}
     </button>
   );
