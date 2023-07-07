eda80e707edbcf56e887342172562cd5071f0f08
E-Albert <magicspoon123@gmail.com>
2023-07-07 11:42:47 -0400

    tailwind css installed

--- src/components/UI/StartPage.js
+++ src/components/UI/StartPage.js
@@ -3,7 +3,7 @@ import Button from "./Button";
 function StartPage(props) {
     return (
       <div className="homepage">
-        <h1>Poké-Race</h1>
+        <h1 className="text-3xl">Poké-Race</h1>
         <Button onClick={props.removeStartPage}>Start</Button>
       </div>
     );
