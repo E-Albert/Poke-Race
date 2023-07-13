3cb022a5364040fb66c6efcdb8f591fd835dbedd
E-Albert <magicspoon123@gmail.com>
2023-07-13 15:51:29 -0400

    header and instruction modal backdrop styled

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -2,10 +2,10 @@ import './Header.css'
 
 function Header() {
     return (
-        <div className='header'>
-            <h1>Poké-Race</h1>
-        </div>
-    )
+      <div className="text-center bg-gradient-to-r from-cyan-500 to-blue-500 h-10 w-screen text-slate-200 flex items-center justify-center mb-10">
+        <h1 className='text-2xl'>Poké-Race</h1>
+      </div>
+    );
 }
 
 export default Header
--- src/components/UI/InstructionsModal.js
+++ src/components/UI/InstructionsModal.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 function InstructionsModal(props) {
   return (
     <div>
-      <div className="backdrop"></div>
+      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black/[.75]"></div>
       <div className="bg-blue-50 text-center z-50 w-2/5 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl p-10">
         <h2 className="text-2xl">Welcome to Poké-Race</h2>
         <br />
