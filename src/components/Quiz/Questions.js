5cef0f446da4695ad69197ef306679853438ee0c
E-Albert <magicspoon123@gmail.com>
2023-07-18 14:43:12 -0400

    added 20 more questions

--- src/components/Quiz/Questions.js
+++ src/components/Quiz/Questions.js
@@ -1,61 +1,182 @@
+let pokeQuestions = [
+  {
+    title: "What type of Pokemon is Squirtle?",
+    choices: ["Fairy", "Water", "Grass", "Normal"],
+    answer: "Water",
+  },
+  {
+    title: "Who was Ash's starter Pokemon?",
+    choices: ["Pikachu", "Geodude", "Charmander", "Caterpie"],
+    answer: "Pikachu",
+  },
+  {
+    title: "Which Pokemon has a burning tail?",
+    choices: ["Charmander", "Weedle", "Pidgey", "Vulpix"],
+    answer: "Charmander",
+  },
+  {
+    title: "Which of the following is Grass type Pokemon?",
+    choices: ["Jigglypuff", "Dugtrio", "Vileplume", "Machoke"],
+    answer: "Vileplume",
+  },
+  {
+    title: "Who is the partner of Jesse from Team Rocket?",
+    choices: ["Cylde", "Samantha", "Deeno", "James"],
+    answer: "James",
+  },
+  {
+    title: "Which member of team rocket also acts as a translator?",
+    choices: ["Brock", "Meowth", "Misty", "Slowbro"],
+    answer: "Meowth",
+  },
+  {
+    title: "Where is Ash Ketchum from?",
+    choices: ["Cerulean City", "Pallet Town", "Fuchsia City", "Indigo Plateau"],
+    answer: "Pallet Town",
+  },
+  {
+    title: "Kadabra evolves into?",
+    choices: ["Abra", "Poliwrath", "Bellsprout", "Alakazam"],
+    answer: "Alakazam",
+  },
+  {
+    title: "How many types of Pokemon are there?",
+    choices: ["18", "20", "21", "16"],
+    answer: "18",
+  },
+  {
+    title: "How does Pikachu evolve into Raichu?",
+    choices: [
+      "In a sandstorm",
+      "During the night time",
+      "With a Thunderstone",
+      "With a Moonstone",
+    ],
+    answer: "With a Thunderstone",
+  },
+  {
+    title: "How many arms does Machamp have?",
+    choices: ["4", "6", "2", "3"],
+    answer: "4",
+  },
+  {
+    title: "How can you break a Ditto's disguise?",
+    choices: [
+      "Defeat it in battle",
+      "Give it a berry",
+      "You can't",
+      "Make it laugh",
+    ],
+    answer: "Make it laugh",
+  },
+  {
+    title:
+      "What do you need to travel from Vermilion City to One, Two, and Three Islands?",
+    choices: ["A Super Rod", "An S.S Ticket", "A Tri-Pass", "A Card Key"],
+    answer: "A Tri-Pass",
+  },
+  {
+    title: "Which two towns are connected by the Cycling Road?",
+    choices: [
+      "Vermilion City and Saffron City",
+      "Fuschia City and Vermilion City",
+      "Fuschia City and Celadon City",
+      "Saffron City and Celadon City",
+    ],
+    answer: "Fuschia City and Celadon City",
+  },
+  {
+    title: "Which town is Koga the Gym Leader of?",
+    choices: ["Lavender Town", "Fuschia City", "Celadon City", "Cerulean City"],
+    answer: "Fuschia City",
+  },
+  {
+    title:
+      "What item do you need to identify the ghost Pokemon in the Pokemon Tower in Lavender Town?",
+    choices: [
+      "A Key Card",
+      "A Basement Key",
+      "A Silph Scope",
+      "A Yellow Flute",
+    ],
+    answer: "A Silph Scope",
+  },
+  {
+    title: "Victory Road is actually not a road. What is it?",
+    choices: [
+      "A Mountain",
+      "A Cave",
+      "The Gates that lead to the Indigo Plateau",
+      "The question is wrong",
+    ],
+    answer: "A Cave",
+  },
+  {
+    title: "What does Vulpix evolve into?",
+    choices: ["Ninetales", "Geodude", "Raichu", "Mankey"],
+    answer: "Ninetales",
+  },
+  {
+    title: "What was the first Pokemon caught by Ash Ketchum?",
+    choices: ["Caterpie", "Pikachu", "Clefairy", "Magikarp"],
+    answer: "Caterpie",
+  },
+  {
+    title: "What is the most common type of Pokemon?",
+    choices: ["Fire", "Ice", "Grass", "Water"],
+    answer: "Water",
+  },
+  {
+    title:
+      "How many Gym Badges must a trainer collect before challenging the Elite Four?",
+    choices: ["6", "8", "12", "13"],
+    answer: "8",
+  },
+  {
+    title: "Which type of attack is super effective against water types?",
+    choices: ["Ice", "Ground", "Fire", "Grass"],
+    answer: "Grass",
+  },
+  {
+    title: "Green is the predominant color for what type?",
+    choices: ["Flying", "Grass", "Rock", "Water"],
+    answer: "Grass",
+  },
+  {
+    title: "Which of the following is a Fairy Pokemon?",
+    choices: ["Zubat", "Clefairy", "Sandslash", "Vulpix"],
+    answer: "Grass",
+  },
+  {
+    title: "Espeon is what type of Pokemon?",
+    choices: ["Ghost", "Ground", "Psychic", "Electric"],
+    answer: "Psychic",
+  },
+  {
+    title: "Which Pokemon evolves into Arbok",
+    choices: ["Nidorina", "Nidoran", "Ekans", "Nidoqueen"],
+    answer: "Ekans",
+  },
+  {
+    title: "Who wants to be the best Pokemon Master ever?",
+    choices: ["Paul", "Ash", "Dawn", "Brock"],
+    answer: "Ash",
+  },
+  {
+    title: "Which green Pokemon evolves into Ivysaur?",
+    choices: ["Bulbasaur", "BellSprout", "Oddish", "Scyther"],
+    answer: "Bulbasaur",
+  },
+  {
+    title: "Which of the following is a specialist Rock type trainer?",
+    choices: ["James", "Ash", "Misty", "Brock"],
+    answer: "Brock",
+  },
+  {
+    title: "The R on the Team Rocket costume is what color?",
+    choices: ["Red", "Yellow", "Pink", "Orange"],
+    answer: "Red",
+  },
+];
 
-  let pokeQuestions = [
-    {
-      title: "What type of Pokemon is Squirtle?",
-      choices: ["Fairy", "Water", "Grass", "Normal"],
-      answer: "Water",
-    },
-    {
-      title: "Who was Ash's starter Pokemon?",
-      choices: ["Pikachu", "Geodude", "Charmander", "Caterpie"],
-      answer: "Pikachu",
-    },
-    {
-      title: "Which Pokemon has a burning tail?",
-      choices: ["Charmander", "Weedle", "Pidgey", "Vulpix"],
-      answer: "Charmander",
-    },
-    {
-      title: "Which of the following is Grass type Pokemon?",
-      choices: ["Jigglypuff", "Dugtrio", "Vileplume", "Machoke"],
-      answer: "Vileplume",
-    },
-    {
-      title: "Who is the partner of Jesse from Team Rocket?",
-      choices: ["Cylde", "Samantha", "Deeno", "James"],
-      answer: "James",
-    },
-    {
-      title: "Which member of team rocket also acts as a translator?",
-      choices: ["Brock", "Meowth", "Misty", "Slowbro"],
-      answer: "Meowth",
-    },
-    {
-      title: "Where is Ash Ketchum from?",
-      choices: [
-        "Cerulean City",
-        "Pallet Town",
-        "Fuchsia City",
-        "Indigo Plateau",
-      ],
-      answer: "Pallet Town",
-    },
-    {
-      title: "Kadabra evolves into?",
-      choices: ["Abra", "Poliwrath", "Bellsprout", "Alakazam"],
-      answer: "Alakazam",
-    },
-    {
-      title: "How many types of Pokemon are there?",
-      choices: ["18", "20", "21", "16"],
-      answer: "18",
-    },
-    {
-        title: "How does Pikachu evolve into Raichu?",
-        choices: ["In a sandstorm", "During the night time", "With a Thunderstone", "With a Moonstone"],
-        answer: "With a Thunderstone",
-    },
-  ];
-   
-
-export default pokeQuestions
+export default pokeQuestions;
